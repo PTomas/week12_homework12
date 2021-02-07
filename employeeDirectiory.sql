4ac4d30ddeda0cc63340605f9aed0009c1bef2bf
Patrick Tomas <p.tomas14@gmail.com>
2021-02-06 17:57:22 -0800

    input message

--- employeeDirectiory.sql
+++ employeeDirectiory.sql
@@ -2,10 +2,18 @@ CREATE DATABASE employeeDatabase;
 
 
 CREATE TABLE employees (
-    employeeID int NOT NULL PRIMARY KEY,
+	ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     firstName varchar(255),
 	lastName varchar(255),
     Title varchar(255),
     Department varchar(255)
 );
 
+INSERT INTO employee
+    (firstName, lastName, Title, Department)
+VALUES
+    ('John', 'Doe', "Lead Software Engineer", "Engineering"),
+    ('Mike', 'Chan', "Lead Engineer", "Engineering"),
+    ('Joe', 'Ming', "Accountant", "Finance"),
+	('Bill', 'Williams', "Manager", "Manager"),
+    ('Tom', 'Stevenson', "Lawyer", "Legal")
