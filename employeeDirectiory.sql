65d0af0d79ff7bbce88a3425fa8e90c283b534f7
Patrick Tomas <p.tomas14@gmail.com>
2021-02-06 17:28:23 -0800

    input message

---
+++ employeeDirectiory.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE employeeDatabase;
+
+
+CREATE TABLE employees (
+    employeeID int NOT NULL PRIMARY KEY,
+    firstName varchar(255),
+	lastName varchar(255),
+    Title varchar(255),
+    Department varchar(255)
+);
+
